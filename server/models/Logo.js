a22702fc1c5078491ac6ef241dbe063bdfc3c902
Kathy Martinez <katherynymartinez@gmail.com>
2020-04-06 20:36:44 -0400

    updated the DB's Logo Schema to include background color, border color, border radius, etc...

--- server/models/Logo.js
+++ server/models/Logo.js
@@ -4,7 +4,13 @@ var LogoSchema = new mongoose.Schema({
   id: String,
   text: String,
   color: String,
-  fontSize: { type: Number, min: 2, max: 144 },
+  fontSize: {type: Number, min: 2, max: 144},
+  backgroundColor: String,
+  borderColor: String,
+  borderRadius: {type: Number, min: 0, max: 100},
+  borderWidth: {type: Number, min: 0, max: 100},
+  padding: {type: Number, min: 0, max: 100},
+  margin: {type: Number, min: 0, max: 100},
   lastUpdate: { type: Date, default: Date.now },
 });
 
