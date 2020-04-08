8834e37dcda3abf22ecbe4d32a67f6545ac94d1a
Kathy Martinez <katherynymartinez@gmail.com>
2020-04-08 19:02:18 -0400

    added browser validation functionality to controls

--- client/src/components/EditSidebar.js
+++ client/src/components/EditSidebar.js
@@ -18,47 +18,47 @@ class EditSidebar extends Component {
                     <div className="form-group">
                         <label htmlFor="text">Text:</label>
                         <input type="text" className="form-control" name="text" onChange={this.handleChange} 
-                        placeholder="Text" value={this.state.text} ref={this.props.textRef}/>
+                        placeholder="Text" value={this.state.text} ref={this.props.textRef} required/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="color">Color:</label>
                         <input type="color" className="form-control" name="color" onChange={this.handleChange} 
-                        placeholder="Color" value={this.state.color} ref={this.props.colorRef}/>
+                        placeholder="Color" value={this.state.color} ref={this.props.colorRef} required/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="fontSize">Font Size:</label>
                         <input type="number" min="4" max="150" className="form-control" name="fontSize" onChange={this.handleChange} 
-                        placeholder="Font Size" value={this.state.fontSize} ref={this.props.fontSizeRef}/>
+                        placeholder="Font Size" value={this.state.fontSize} ref={this.props.fontSizeRef} required/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="backgroundColor"> Background Color:</label>
                         <input type="color" className="form-control" name="backgroundColor" onChange={this.handleChange} 
-                        placeholder="Background Color" value={this.state.backgroundColor} ref={this.props.backgroundColorRef}/>
+                        placeholder="Background Color" value={this.state.backgroundColor} ref={this.props.backgroundColorRef} required/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="borderColor"> Border Color:</label>
                         <input type="color" className="form-control" name="borderColor" onChange={this.handleChange} 
-                        placeholder="Border Color" value={this.state.borderColor} ref={this.props.borderColorRef}/>
+                        placeholder="Border Color" value={this.state.borderColor} ref={this.props.borderColorRef} required/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="borderRadius">Border Radius:</label>
                         <input type="number" min="0" max="100" className="form-control" name="borderRadius" onChange={this.handleChange} 
-                        placeholder="Border Radius" value={this.state.borderRadius} ref={this.props.borderRadiusRef}/>
+                        placeholder="Border Radius" value={this.state.borderRadius} ref={this.props.borderRadiusRef} required/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="borderWidth">Border Width:</label>
                         <input type="number" min="0" max="100" className="form-control" name="borderWidth" onChange={this.handleChange} 
-                        placeholder="Border Width" value={this.state.borderWidth} ref={this.props.borderWidthRef}/>
+                        placeholder="Border Width" value={this.state.borderWidth} ref={this.props.borderWidthRef} required/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="padding">Padding:</label>
                         <input type="number" min="0" max="100" className="form-control" name="padding" onChange={this.handleChange} 
-                        placeholder="Padding" value={this.state.padding} ref={this.props.paddingRef}/>
+                        placeholder="Padding" value={this.state.padding} ref={this.props.paddingRef} required/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="margin">Margin:</label>
                         <input type="number" min="0" max="100" className="form-control" name="margin" onChange={this.handleChange} 
-                        placeholder="Margin" value={this.state.margin} ref={this.props.marginRef}/>
+                        placeholder="Margin" value={this.state.margin} ref={this.props.marginRef} />
                     </div>
                 </div>
                 <div className="col-sm-9">
